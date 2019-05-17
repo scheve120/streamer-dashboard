e2be205563e0a1c2d7605f4f61165b1f58ffe29d
scheve120 <remotec@live.nl>
2019-05-17 12:17:29 +0200

    The last clean up.

--- includes/email/PHP-Mail-handler.php
+++ includes/email/PHP-Mail-handler.php
@@ -1,31 +1,43 @@
 <?php
-// require_once './includes/user/registration.php';
-function send_email ($user_mailing) {
+
+/**
+ * @file
+ * PHP Email script.
+ */
+
+/**
+ * @file
+ * Old email function Meby setting it for smtp.
+ */
+function send_email($user_mailing) {
   $to = $user_mailing['email'];
   $subject = "Test of email werkt";
   $from = "broworkspace.nl";
-  $text = "Gefeliciteerd". $user_mailing['name']. "je ben nu lid van <a href=Browowkrspace.nl>Browowkrspace</a>";
-  $text = wordwrap($text,70);
-  // mail($to,$subject,$text);
+  $text = "Gefeliciteerd" . $user_mailing['name'] . "je ben nu lid van <a href=Browowkrspace.nl>Browowkrspace</a>";
+  $text = wordwrap($text, 70);
 }
 
-function email_sender ($data) {
+/**
+ * @file
+ * Send email function.
+ */
+function email_sender($data) {
   global $site_domain;
-  
-  // print_r($data['email']);
+
+  // Creating the email variables.
   $username = $data['username'];
   $username = htmlspecialchars($username);
   $to = $data['email_to'];
   $subject = $data['email_subject'];
-  $from = $data['from'].$site_domain;
+  $from = $data['from'] . $site_domain;
 
-  // Send content with html
+  // Send content with html.
   $headers  = 'MIME-Version: 1.0' . "\r\n";
   $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
   $headers .= "From: $from test location from the headers";
   $text = $data["email_text"];
 
-  $text = wordwrap($text,70);
+  $text = wordwrap($text, 70);
 
   mail($to, $subject, $text, $headers);
 
